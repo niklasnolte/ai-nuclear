a70b4b7e43b7b91d69e24627621a9548aaa09caf
Niklas Nolte <niklasnolte@users.noreply.github.com>
2023-03-08 17:13:24 -0500

    modular + split up model on top (#2)
    * modular arithmetic + train frac determinstic
    
    * add splitup model and results to gitignore
    
    Co-authored-by: kitouni <28654657+okitouni@users.noreply.github.com>
    
    * changed d_model for splitup & add tracking nparams
    
    * fix bug about when to do wandb things
    
    ---------
    
    Co-authored-by: kitouni <28654657+okitouni@users.noreply.github.com>

--- loss.py
+++ loss.py
@@ -4,6 +4,12 @@ from sklearn.preprocessing import QuantileTransformer
 import argparse
 import math
 
+
+def random_softmax(shape, scale=1):
+    x = torch.rand(shape)
+    return torch.softmax(scale * x, dim=-1) * x.shape[-1]
+
+
 def weight_by_task(output_map: dict, args: argparse.Namespace) -> torch.Tensor:
     weights = []
     for target_name in output_map.keys():
@@ -17,7 +23,10 @@ def weight_by_task(output_map: dict, args: argparse.Namespace) -> torch.Tensor:
 
 
 def loss_by_task(
-    output: torch.Tensor, targets: torch.Tensor, output_map: dict, config: argparse.Namespace
+    output: torch.Tensor,
+    targets: torch.Tensor,
+    output_map: dict,
+    config: argparse.Namespace,
 ) -> torch.Tensor:
     """
     output: [batch_size, output_dim]
@@ -49,7 +58,8 @@ def loss_by_task(
             output_column += 1
     return loss
 
-def get_balanced_accuracy(output:torch.Tensor, target:torch.Tensor):
+
+def get_balanced_accuracy(output: torch.Tensor, target: torch.Tensor):
     """
     output: [batch_size, output_dim]
     target: [batch_size]
@@ -61,13 +71,14 @@ def get_balanced_accuracy(output:torch.Tensor, target:torch.Tensor):
     n_classes = len(target.unique())
     class_occurrences = torch.bincount(target)
     class_weight = 1 / class_occurrences.float() / n_classes
-    return ((output == target).float() * class_weight[target] ).sum()
+    return ((output == target).float() * class_weight[target]).sum()
+
 
 def metric_by_task(
     output: torch.Tensor,
     targets: torch.Tensor,
     output_map: dict,
-    config : argparse.Namespace,
+    config: argparse.Namespace,
     qt: QuantileTransformer = None,
 ) -> torch.Tensor:
     """
@@ -90,40 +101,43 @@ def metric_by_task(
     metrics = torch.zeros(len(target_names))
 
     # classification metrics [acc]
-    classification_targets = [t for t in target_names if t in config.TARGETS_CLASSIFICATION]
+    classification_targets = [
+        t for t in target_names if t in config.TARGETS_CLASSIFICATION
+    ]
     target_column = 0
     for target_name in classification_targets:
         mask = ~torch.isnan(targets[:, target_column])
         masked_target = targets[:, target_column][mask].long()
         size = output_map[target_name]
         out = output[:, output_column : output_column + size]
-        metrics[target_column] = (
-            100 * get_balanced_accuracy(out[mask], masked_target)
-        )
+        metrics[target_column] = 100 * get_balanced_accuracy(out[mask], masked_target)
         output_column += size
         target_column += 1
 
     # regression metrics [rmse]
     regression_targets = [t for t in target_names if t in config.TARGETS_REGRESSION]
     if len(regression_targets) > 0 and qt is not None:
-        targets[:, target_column:] = torch.tensor(qt.inverse_transform(targets[:, target_column:].cpu()))
-        output[:, output_column:] = torch.tensor(qt.inverse_transform(output[:, output_column:].cpu()))
+        targets[:, target_column:] = torch.tensor(
+            qt.inverse_transform(targets[:, target_column:].cpu())
+        )
+        output[:, output_column:] = torch.tensor(
+            qt.inverse_transform(output[:, output_column:].cpu())
+        )
 
     for target_name in regression_targets:
         mask = ~torch.isnan(targets[:, target_column])
         masked_target = targets[:, target_column][mask]
         out = output[:, output_column]
-        metrics[target_column] = F.mse_loss(
-            out[mask], masked_target.float()
-        ).sqrt()
+        metrics[target_column] = F.mse_loss(out[mask], masked_target.float()).sqrt()
         output_column += 1
         target_column += 1
 
     return metrics
 
+
 def test_loss_by_task():
     output = torch.tensor([[0.1, 0.9, 0.1], [0.9, 0.1, 0.9]])
-    targets = torch.tensor([[1., 0.0], [0.0, 1.0]])
+    targets = torch.tensor([[1.0, 0.0], [0.0, 1.0]])
     output_map = {"a": 2, "b": 1}
     config = argparse.Namespace()
     config.TARGETS_CLASSIFICATION = ["a"]
@@ -134,26 +148,30 @@ def test_loss_by_task():
     assert loss[1] == F.mse_loss(output[:, 2], targets[:, 1].float())
     print("test_loss_by_task passed")
 
+
 def test_get_balanced_accuracy():
     # test with class weights that are not uniform
     output = torch.tensor([[0.1, 0.9], [0.9, 0.1], [0.1, 0.9]])
     targets = torch.tensor([1.0, 1.0, 0.0])
     acc = get_balanced_accuracy(output, targets)
-    assert math.isclose(acc.item(), .25)
+    assert math.isclose(acc.item(), 0.25)
     print("test_get_accuracy passed")
+
 
 def test_metric_by_task():
     # test metrics with class weights that are not uniform
 
     output = torch.tensor([[0.1, 0.9, 0.1], [0.9, 0.1, 0.9]])
-    targets = torch.tensor([[1., 0.0], [0.0, 1.0]])
+    targets = torch.tensor([[1.0, 0.0], [0.0, 1.0]])
     output_map = {"a": 2, "b": 1}
     config = argparse.Namespace()
     config.TARGETS_CLASSIFICATION = ["a"]
     config.TARGETS_REGRESSION = ["b"]
     metrics = metric_by_task(output, targets, output_map, config)
     assert metrics.shape == (2,)
-    assert metrics[0] == 100 * get_balanced_accuracy(output[:, :2], targets[:, 0].long())
+    assert metrics[0] == 100 * get_balanced_accuracy(
+        output[:, :2], targets[:, 0].long()
+    )
     assert metrics[1] == F.mse_loss(output[:, 2], targets[:, 1].float()).sqrt()
     print("test_metric_by_task passed")
 

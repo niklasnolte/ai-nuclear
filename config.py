bcf2a93670e7219504eb1b53d8f0f1c4901a8299
nnolte <niklas.nolte@cern.ch>
2023-03-23 13:27:24 -0400

    big model go brrr

--- config.py
+++ config.py
@@ -9,26 +9,32 @@ class Task(Enum):
         # FULL_dimnreg2_pcaalpha-1.5_wd0.003_lr0.1_epochs30000_trainfrac0.8_hiddendim256_seed1_modelbaseline_targetsclassificationNone_targetsregressionz:1-n:1-binding_energy:1-radius:1
         dict(
             MODEL=["baseline"],
-            WD=[3e-3],  # first one seems to be best
+            WD=[5e-2],  # first one seems to be best
             LR=[1e-2],
-            EPOCHS=[10000],
-            TRAIN_FRAC=[0.8],
-            HIDDEN_DIM=[256],
-            DIMREG_COEFF=[0., 2.0, 1.0, 0.5],
-            DIMREG_EXP=[-1.5, -1.0],  # power to weight indices in dimn regularization
-            SEED=[0, 1, 2],
+            EPOCHS=[1000000],
+            TRAIN_FRAC=[0.9],
+            HIDDEN_DIM=[2048],
+            DIMREG_COEFF=[0.],
+            DIMREG_EXP=[-1.5],  # power to weight indices in dimn regularization
+            SEED=[0],
             RANDOM_WEIGHTS=[0.], # level of entropy in randomness. 0 is uniform. 1000 is random one hot.
             TARGETS_CLASSIFICATION=[
                 {},
                 {"stability": 1, "parity": 1, "spin": 1, "isospin": 1},
             ],
             TARGETS_REGRESSION=[
-                {
-                    "z": 1,
-                    "n": 1,
-                    "binding_energy": 1,
-                    "radius": 1,
-                },
+                # {
+                #     "z": 1,
+                #     "n": 1,
+                #     "binding_energy": 1,
+                #     "radius": 1,
+                #     # "qa": 1,
+                #     # "qbm": 1,
+                #     # "qbm_n": 1,
+                #     # "qec": 1,
+                #     # "sn": 1,
+                #     # "sp": 1,
+                # },
                 {
                     "z": 1,
                     "n": 1,

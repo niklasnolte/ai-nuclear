e78dc225539a9069dd8e3921f2d8e8e2d4379bc2
niklasnolte <niklas.nolte@cern.ch>
2023-03-29 10:27:35 -0600

    plot model

---
+++ plot_model.py
@@ -0,0 +1,95 @@
+import torch
+from data import prepare_nuclear_data, prepare_modular_data, train_test_split
+from config import Task
+import matplotlib.pyplot as plt
+from model import get_model_and_optim
+from config_utils import parse_arguments_and_get_name
+import os
+
+# load TASK from env
+TASK = Task[os.environ.get("TASK")]
+
+args, name = parse_arguments_and_get_name(TASK)
+torch.manual_seed(args.SEED)
+
+if TASK == Task.FULL or TASK == Task.DEBUG:
+    data = prepare_nuclear_data(args)
+elif TASK == Task.MODULAR:
+    data = prepare_modular_data(args)
+
+print(args)
+
+def load(path):
+    return torch.load(path, map_location=torch.device('cpu'))
+
+
+basedir = os.path.join(args.ROOT, name)
+modelpath = os.path.join(basedir, "model_FULL_best.pt")
+model, _ = get_model_and_optim(data, args)
+model.load_state_dict(load(modelpath))
+model.eval()
+
+
+DEVICE = args.DEV
+train_mask, val_mask = train_test_split(
+    data, train_frac=args.TRAIN_FRAC, seed=args.SEED
+)
+
+def get_idx(data, target):
+    output_map = data.output_map
+    if target not in output_map:
+        raise ValueError(f"Target {target} not in output map")
+    target_size = output_map[target]
+    if target_size > 1:
+        raise ValueError(f"Target {target} is not a scalar")
+    target_idx = 0
+    for prev_target, size in output_map.items():
+        if prev_target == target:
+            return target_idx
+        target_idx += size
+
+
+def plot(target="binding_energy", split="both", rescale=False):
+    true = data.y
+    with torch.no_grad(): pred = model(data.X)
+    if rescale:
+        true = torch.tensor(data.regression_transformer.inverse_transform(true))
+        pred = torch.tensor(data.regression_transformer.inverse_transform(pred))
+    target_idx = get_idx(data, target)
+    target_true = true[:, target_idx]
+    target_pred = pred[:, target_idx]
+    if split == "train":
+        split_mask = train_mask
+    elif split == "val":
+        split_mask = val_mask
+    elif split == "both":
+        split_mask = train_mask | val_mask
+    else:
+        raise ValueError(f"Unknown split {split}")
+    nanmask = ~target_true.isnan()
+    mask = split_mask & nanmask
+
+    _, axes = plt.subplots(2, 2, figsize=(20, 14))
+    axes = axes.flatten()
+    for ax, target in zip(axes, [target_true, target_true - target_pred]):
+        sc = ax.scatter(data.X[:, 0][mask], data.X[:, 1][mask], c=target[mask], s=10, cmap="bwr")
+        ax.set_xlabel("Z")
+        ax.set_ylabel("N")
+        #center the colorbar around 0
+        if ax == axes[1]:
+          max_ = max(abs(sc.get_clim()[0]), abs(sc.get_clim()[1]))
+          sc.set_clim(-max_, max_)
+        plt.colorbar(sc, ax=ax)
+    for ax, title in zip(axes, ["True", "True - Pred"]):
+        ax.set_title(title)
+    for ax, x in zip(axes[2:], data.X.T):
+        ax.scatter(x, target_true, s=10, label="True")
+        ax.scatter(x, target_pred, s=10, label="Pred")
+        ax.set_xlabel("N" if ax == axes[2] else "Z")
+        ax.set_ylabel("Binding energy")
+        ax.legend()
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    plot("binding_energy", split="train", rescale=True)

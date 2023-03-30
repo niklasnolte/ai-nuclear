c36ec67aa550ead123b4f4c650e93bd9a4fde4ce
kitouni <28654657+okitouni@users.noreply.github.com>
2023-03-30 15:51:27 -0400

    [config] run with nice circle

--- model_symbols.py
+++ model_symbols.py
@@ -6,14 +6,20 @@ from torch import nn
 from typing import Union, Iterable
 from data import train_test_split_exact
 import tqdm
-
+import mup
+from model import BaselineModel, make_mup
+from functools import partial
+from regularization import forward_with_reg
+import random
 # %%
+
 class BaselineModel(Base):
     def __init__(
         self,
         vocab_size: Union[int, Iterable],
         hidden_dim: int,
         output_dim: int,
+        factor=2,
         dropout: float = 0.0,
     ):
         """
@@ -26,14 +32,14 @@ class BaselineModel(Base):
         super().__init__(vocab_size, hidden_dim)
 
         self.nonlinear = nn.Sequential(
-            nn.Linear(3 * hidden_dim, hidden_dim),
+            nn.Linear(factor * hidden_dim, hidden_dim),
             nn.SiLU(),
-            nn.Linear(hidden_dim, hidden_dim),
+            # nn.Linear(hidden_dim, hidden_dim),
+            # nn.SiLU(),
+            ResidualBlock(hidden_dim, dropout=dropout),
             nn.SiLU(),
-            # ResidualBlock(hidden_dim, dropout=dropout),
-            # nn.SiLU(),
-            # ResidualBlock(hidden_dim, dropout=dropout),
-            # nn.SiLU(),
+            ResidualBlock(hidden_dim, dropout=dropout),
+            nn.SiLU(),
             nn.Linear(hidden_dim, hidden_dim),
             nn.SiLU(),
         )
@@ -43,6 +49,8 @@ class BaselineModel(Base):
         x = self.embed_input(x, embs)
         x = self.nonlinear(x)  # [ batch_size, hidden_dim ]
         return self.readout(x)  # [ batch_size, output_dim ]
+
+
 
 def train_test_split_exact(size, train_frac=0.8, seed=0, force_train_mask=None):
     torch.manual_seed(seed)
@@ -80,7 +88,7 @@ P = 53
 X = torch.cartesian_prod(
     torch.arange(P), torch.arange(P), torch.arange(len(operations))
 )
-# order by operation
+
 X = torch.cat([X[X[:, 2] == i] for i in range(len(operations))])
 y = torch.zeros(len(X))
 TRAIN_FRAC = 0.8
@@ -97,11 +105,12 @@ for i, (a, b, op) in enumerate(X):
 mask_train, mask_test = [], []
 seeds = [0, 1, 2]
 for rng, op in zip(seeds, operations):
-    if op == "multiply":
-        mask_train_op = torch.ones(P * P, dtype=torch.bool)
-        mask_test_op = torch.zeros(P * P, dtype=torch.bool)
-    else:
-        mask_train_op, mask_test_op = train_test_split_exact(P * P, train_frac=TRAIN_FRAC, seed=rng)
+    # if op == "multiply":
+    #     mask_train_op = torch.ones(P * P, dtype=torch.bool)
+    #     mask_test_op = torch.zeros(P * P, dtype=torch.bool)
+    # else:
+    #     mask_train_op, mask_test_op = train_test_split_exact(P * P, train_frac=TRAIN_FRAC, seed=rng)
+    mask_train_op, mask_test_op = train_test_split_exact(P * P, train_frac=TRAIN_FRAC, seed=rng)
     mask_train.append(mask_train_op)
     mask_test.append(mask_test_op)
 mask_train = torch.cat(mask_train)
@@ -118,38 +127,51 @@ for i, op in enumerate(operations):
     ax[i].set_xticks([])
     ax[i].set_yticks([])
 
-
 # %%
-model = BaselineModel(vocab_size=(P + len(operations)), hidden_dim=64, output_dim=P)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4)
-criterion = nn.CrossEntropyLoss()
+# TRAIN MODEL
+def train_model(epochs=100, hidden_dim=128, lr=1e-2, weight_decay=1, reg=1, amsgrad=True):
+    torch.manual_seed(0)
+    model_fn = partial(BaselineModel, vocab_size=(P + len(operations)), output_dim=P, factor=3)
+    model = make_mup(model_fn, hidden_dim=hidden_dim)
+    param_groups = [
+        {"params": model.emb.parameters(), "weight_decay": weight_decay},
+        {"params": model.nonlinear.parameters(), "weight_decay": weight_decay},
+        {"params": model.readout.parameters(), "weight_decay": weight_decay},
+    ]
+    # optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    optimizer = mup.MuAdam(param_groups, lr=lr, amsgrad=True)
+    criterion = nn.CrossEntropyLoss()
 
-print("T:  loss |  acc  || V:  loss |  acc")
-bar = tqdm.trange(20000)
-for epoch in bar:
-    model.train()
-    optimizer.zero_grad()
-    out = model(X[mask_train])
-    loss_train = criterion(out, y[mask_train].long())
-    loss_train.backward()
-    optimizer.step()
-    acc_train = accuracy(out, y[mask_train].long())
-    task_accs_train = get_task_accs(out, y[mask_train].long(), X[mask_train])
+    print("T:  loss |  acc  || V:  loss |  acc")
+    bar = tqdm.trange(epochs)
+    for epoch in bar:
+        model.train()
+        optimizer.zero_grad()
+        out = model(X[mask_train])
+        # out_reg = forward_with_reg(model, random.randint(1,hidden_dim-1), X[mask_train])
+        loss_train = criterion(out, y[mask_train].long())
+        # loss_train += reg * criterion(out_reg, y[mask_train].long())
+        loss_train.backward()
+        optimizer.step()
+        acc_train = accuracy(out, y[mask_train].long())
+        task_accs_train = get_task_accs(out, y[mask_train].long(), X[mask_train])
 
-    model.eval()
-    out = model(X[mask_test])
-    loss_test = criterion(out, y[mask_test].long())
-    acc_test = accuracy(out, y[mask_test].long())
-    msg = (
-        f"T: {loss_train:.3f} | {acc_train:.3f} || V: {loss_test:.3f} | {acc_test:.3f}"
-    )
-    bar.set_description_str(msg)
-    task_accs_test = get_task_accs(out, y[mask_test].long(), X[mask_test])
-    postfix = " | ".join([f"{op}: {acc:.2f}" for op, acc in task_accs_test.items()])
-    bar.set_postfix_str(postfix)
-
-
+        model.eval()
+        out = model(X[mask_test])
+        loss_test = criterion(out, y[mask_test].long())
+        acc_test = accuracy(out, y[mask_test].long())
+        msg = (
+            f"T: {loss_train:.3f} | {acc_train:.3f} || V: {loss_test:.3f} | {acc_test:.3f}"
+        )
+        bar.set_description_str(msg)
+        task_accs_test = get_task_accs(out, y[mask_test].long(), X[mask_test])
+        postfix = " | ".join([f"{op}: {acc:.2f}" for op, acc in task_accs_test.items()])
+        bar.set_postfix_str(postfix)
+    return model
 # %%
+model = train_model(epochs=5000, hidden_dim=128, lr=1e-2, weight_decay=1e-3, amsgrad=True)
+# %%
+
 # Now let's plot the embeddings
 from sklearn.decomposition import PCA
 from matplotlib.cm import get_cmap
@@ -165,12 +187,16 @@ colors = get_cmap("viridis")(np.linspace(0, 1, P))
 for i in range(P):
     ax.text(embs[i, 0], embs[i, 1], str(i), color=colors[i], fontsize=8)
 for i, op in enumerate(operations):
-    # offset operations by 5 %
-    ax.text(embs[P + i, 0] * 2.15 ** i , embs[P + i, 1]* 2.15 ** i, op, color="k", fontsize=8)
+    ax.text(embs[P + i, 0] , embs[P + i, 1]* 2.15 * (i+1), op, color="k", fontsize=8)
 ax.plot(embs[:P, 0], embs[:P, 1], "k", alpha=0.5, linewidth=0.5)
-ax.set_xticks([])
-ax.set_yticks([])
+# ax.set_xticks([])
+# ax.set_yticks([])
 ax.set_title("Embeddings")
 
+
+# %%
+for i, op in enumerate(operations):
+    print(op)
+    print(embs[P + i, :])
 
 # %%

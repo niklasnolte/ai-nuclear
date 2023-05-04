correctly implement scaling by A for binding energies, gradient clipping, fix logging with log freq

log.py
@@ -7,7 +7,6 @@ class Logger:
     def __init__(self, args, model ):
         self.args = args
         # save args
-        self.epoch = 0
         self.model = model
         # FIXME: this is a hack to avoid logging outside train.py
         if hasattr(args, "basedir"):
@@ -21,37 +20,35 @@ class Logger:
             self.basedir = None
         self.best_loss = float("inf")
 
-    def log(self, metrics=None, epoch=None):
+    def log(self, metrics, epoch):
         if self.basedir is None: return -1
-        epoch = epoch if epoch is not None else self.epoch
-        if epoch % self.args.LOG_FREQ == 0: # TODO check best model every epoch?
-            val_loss = metrics["val_loss_all"]
-            # keep track of the best model
-            if val_loss < self.best_loss:
-                self.best_loss = val_loss
-                self.best_model = self.model.state_dict().copy()
-                if self.args.WANDB:
-                    wandb.run.summary["best_epoch"] = epoch
-                    for i, (target, value) in enumerate(metrics.items()):
-                        if "val" in target:
-                            wandb.run.summary[f"best_{target}"] = value
-                torch.save(
-                    self.best_model, os.path.join(self.basedir, "model_best.pt")
-                )
+        val_loss = metrics["val_loss_all"]
+        # keep track of the best model
+        if val_loss < self.best_loss:
+            self.best_loss = val_loss
+            self.best_model = self.model.state_dict().copy()
             if self.args.WANDB:
-                wandb.log(metrics, step=epoch)
-            else:
-                train_items = [
-                    f"{' '.join(k.split('_')[1:]):<20} | {v:<8.2e}"
-                    for k, v in metrics.items()
-                    if "train" in k
-                ]
-                val_items = [f"{v:<8.2e}" for k, v in metrics.items() if "val" in k]
-                hold_out_items = [f"{v:<8.2e}" for k, v in metrics.items() if "holdout" in k]
-                items = [" | ".join(x) for x in zip(train_items, val_items, hold_out_items)]
-                msg = f"Epoch {epoch:<14} | {'Train':^8} | {'Val':^8} | {'holdout':^8}\n"
-                msg += "\n".join(sorted(items, key=lambda x: x.split(" ")[0]))
-                print(msg)
+                wandb.run.summary["best_epoch"] = epoch
+                for i, (target, value) in enumerate(metrics.items()):
+                    if "val" in target:
+                        wandb.run.summary[f"best_{target}"] = value
+            torch.save(
+                self.best_model, os.path.join(self.basedir, "model_best.pt")
+            )
+        if self.args.WANDB:
+            wandb.log(metrics, step=epoch)
+        else:
+            train_items = [
+                f"{' '.join(k.split('_')[1:]):<20} | {v:<8.2e}"
+                for k, v in metrics.items()
+                if "train" in k
+            ]
+            val_items = [f"{v:<8.2e}" for k, v in metrics.items() if "val" in k]
+            hold_out_items = [f"{v:<8.2e}" for k, v in metrics.items() if "holdout" in k]
+            items = [" | ".join(x) for x in zip(train_items, val_items, hold_out_items)]
+            msg = f"Epoch {epoch:<14} | {'Train':^8} | {'Val':^8} | {'holdout':^8}\n"
+            msg += "\n".join(sorted(items, key=lambda x: x.split(" ")[0]))
+            print(msg)
         if epoch == self.args.EPOCHS - 1 or epoch == 0:
             state_dict_path = os.path.join(self.basedir, f"model_FULL.pt")
             torch.save(self.model.state_dict(), state_dict_path)
@@ -65,5 +62,3 @@ class Logger:
                     self.model.state_dict(),
                     os.path.join(self.basedir, f"model_{epoch}.pt"),
                 )
-
-        self.epoch += 1

train_full.py
@@ -79,7 +79,7 @@ class Trainer:
             for x, y in self.loader:
                 self.train_step(x, y)
             if epoch % self.args.LOG_FREQ == 0:
-                self.val_step(log=True)
+                self.val_step(epoch, log=True)
 
     def train_step(self, X, y):
         self.model.train()
@@ -88,12 +88,16 @@ class Trainer:
         task = X[:, len(self.data.vocab_size) - 1]
         losses, num_samples = self.loss_by_task(task, out, y)
         loss = losses.sum() / num_samples.sum()  # TODO weights?
+        # gradient clipping
+        for param in self.model.parameters():
+          if param.grad is not None:
+            param.grad = torch.clamp(param.grad, -.1, .1)
         loss.backward()
         self.optimizer.step()
         self.scheduler.step()
         return out, losses, num_samples
 
-    def val_step(self, log=False):
+    def val_step(self, epoch, log=False):
         # This serves as the logging step as well
         X, y = self.data.X, self.data.y
         task = self.all_tasks
@@ -111,7 +115,7 @@ class Trainer:
                 metrics_dict.update(m)
 
         if log:
-            self.logger.log(metrics_dict)
+            self.logger.log(metrics_dict, epoch)
         return metrics_dict
 
     def construct_metrics(self, losses, metrics, num_samples, prefix):
@@ -188,7 +192,9 @@ class Trainer:
         else:
             A = self.data.X[:: self.num_tasks, :2].sum(1).view(-1, 1)
         out = torch.from_numpy(out).to(self.args.DEV)
-        out[:, :2] = out[:, :2] * (A)
+        # self.data.output_map gives the size of each output
+        # we want to scale binding, so the binding idx is the sum of all sizes before it
+        out[:, self.data.scaled_idxs] = out[:, self.data.scaled_idxs] * A
         return out
 
     @cached_property
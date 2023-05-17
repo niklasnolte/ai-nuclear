fix data leak problem

data.py
@@ -421,9 +421,8 @@ def prepare_nuclear_data(
         [torch.tensor([*x, task]) for x in X for task in torch.arange(len(output_map))]
     )
     y = y.flatten().view(-1, 1)
-    k_fold_cv_idx = _k_fold_cv(len(y), config.N_FOLDS, seed=config.SEED)
 
-    test_mask = k_fold_cv_idx
+    k_fold_cv_idx = _k_fold_cv(len(y), config.N_FOLDS, seed=config.SEED)
 
     # scale those by A
     binding_idxs = [i for i, x in enumerate(output_map.keys()) if "binding" in x]
@@ -447,9 +446,6 @@ def prepare_nuclear_data(
 
     elif config.TMS != "keep":
         raise ValueError(f"Unknown TMS {config.TMS}")
-
-    if test_mask.sum() == 0:
-        raise ValueError("No test data!")
 
     return Data(
         X.to(config.DEV),

train_full.py
@@ -22,9 +22,9 @@ class TrainLoader:
     def __init__(self, X, y, fold_idxs, batch_size=1024):
         self.X = X
         self.y = y
-        self.fold_idxs = fold_idxs
         self.batch_size = batch_size
         self.randperm = torch.randperm(len(X), device=X.device)
+        self.fold_idxs = fold_idxs[self.randperm]
         self.with_fold(0)
 
     def __iter__(self):
@@ -115,13 +115,12 @@ class Trainer:
 
     def val_step(self, epoch, log=False):
         # This serves as the logging step as well
-        X, y = self.data.X, self.data.y
         task = self.all_tasks
         metrics_dicts = []
         for fold, model in enumerate(self.models):
             model.eval()
             with torch.no_grad():
-                out = model(X)
+                out = model(self.data.X)
                 out_ = self._unscale_output(out.clone())  # reg_targets are rescaled
                 y_ = self.unscaled_y
                 metrics_dict = {}
@@ -130,7 +129,7 @@ class Trainer:
                 masks = {"train": train_mask, "val": val_mask}
                 for name, mask in masks.items():
                     losses, num_samples = self.loss_by_task(
-                        task[mask], out[mask], y[mask]
+                        task[mask], out[mask], self.data.y[mask]
                     )
                     metrics, _ = self.metrics_by_task(task[mask], out_[mask], y_[mask])
                     m = self.construct_metrics(losses, metrics, num_samples, name)